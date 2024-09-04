0863cf53063f37d6e419e2aa45d7d386d6249997
Oskar Nilsson <143490244+oskarnilsson90@users.noreply.github.com>
2024-09-04 11:16:48 +0200

    DatabaseContext and connection

---
+++ Bokregister_API_Oskar/Bokregister_API_Oskar/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Bokregister_API_Oskar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bokregister_API_Oskar.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Bokregister_API_Oskar/Bokregister_API_Oskar/Program.cs
+++ Bokregister_API_Oskar/Bokregister_API_Oskar/Program.cs
@@ -1,6 +1,13 @@
+using Bokregister_API_Oskar.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add DbContext for database operations.
+builder.Services.AddDbContext<DatabaseContext>(option =>
+                                    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
